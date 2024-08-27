cc9175f9f75da85c770cbe6b3aeb166664fb5f47
joel kwihangana <joelkwijoe@gmail.com>
2024-08-27 18:08:02 +0200

    fetch chats using axios

--- client/src/pages/ChatPage.tsx
+++ client/src/pages/ChatPage.tsx
@@ -1,17 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+interface IChats {
+  _id: number;
+  chatName: string;
+  isGroupChat: string;
+}
+
 const ChatPage = () => {
+  const [chats, setChats] = useState<IChats[]>([]);
   const fetchChats = async () => {
     const response = await axios.get("http://localhost:5000/api/chat");
 
     console.log(response);
+    setChats(response.data);
   };
 
   useEffect(() => {
     fetchChats();
   }, []);
-  return <div>ChatPage</div>;
+  return (
+    <div>
+      <h1>List of Chats</h1>
+      <ul>
+        {chats.map((chat) => (
+          <li key={chat._id}>{chat.chatName}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ChatPage;
